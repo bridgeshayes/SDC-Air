cb5b24c1acf73108e559b67f51ee798b60467b00
Emma Long <emmag2623@gmail.com>
2025-03-25 18:35:27 -0500

    login event lilstener

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,51 +6,51 @@ const baseURL = "http://localhost:3000";
 const loginBtn = document.getElementById("btnLogin");
 const registerBtn = document.getElementById("btnRegister");
 
-const responseRegister = fetch(baseURL, {
-  method: "POST",
-  body: {
-    username: "",
-    email: "",
-    password: "",
-    first_name: "",
-    last_name: "",
-    is_host: 0,
+loginBtn.addEventListener('click', async (event) => {
+    const username = document.getElementById('txtUsernameLogin').value
+    const password = document.getElementById('txtPasswordLogin').value
 
-  },
+    const responseLogin = fetch(baseURL + "/login", {
+        method: "POST",
+        body: {
+            username: username,
+            password: password
+        },
+    });
+    
+    if(responseLogin.ok) 
+    {
+        console.log("YIPPEE IT SENT, WE GOT A STATUS 200, and EVERYTHING IS OKAY!");
+    }
+    
+    else if(!responseLogin.ok) 
+    {
+        throw new Error(`Response Status: ${responseLogin.status}`);
+    }
+})
 
-});
-
-if(responseRegister.ok) 
-{
-  console.log("YIPPEE IT SENT, WE GOT A STATUS 200, and EVERYTHING IS OKAY!");
-}
-
-else if(!responseRegister.ok) 
-{
-  throw new Error(`Response Status: ${responseRegister.status}`);
-}
-
-
-const responseLogin = fetch("http://localhost:3000/register", {
-  method: "POST",
-  body: {
-    username: "",
-    email: "",
-    password: "",
-    first_name: "",
-    last_name: "",
-    is_host: 0,
-
-  },
-
-});
-
-if(responseLogin.ok) 
-{
-console.log("YIPPEE IT SENT, WE GOT A STATUS 200, and EVERYTHING IS OKAY!");
-}
-
-else if(!responseLogin.ok) 
-{
-throw new Error(`Response Status: ${responseLogin.status}`);
-}
+registerBtn.addEventListener('click', async (event) => {
+    const responseRegister = fetch(baseURL + "/register", {
+        method: "POST",
+        body: {
+            username: "",
+            email: "",
+            password: "",
+            first_name: "",
+            last_name: "",
+            is_host: 0,
+    
+        },
+    
+    });
+    
+    if(responseRegister.ok) 
+    {
+        console.log("YIPPEE IT SENT, WE GOT A STATUS 200, and EVERYTHING IS OKAY!");
+    }
+    
+    else if(!responseRegister.ok) 
+    {
+        throw new Error(`Response Status: ${responseRegister.status}`);
+    }
+})
